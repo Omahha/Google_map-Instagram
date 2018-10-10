8b0c1908330b55e260bbedd601248789501c6c31
Khanit <omahha7@gmail.com>
2018-10-10 16:00:40 +0900

    t

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
                     type: "GET",
                     dataType: "jsonp",
                     cache: false,
-                    url: "https://maps.googleapis.com/maps/api/geocode/json?address="+ address +"&key=AIzaSyDymcMD7E0irE6FM1uGqzEhVmU5LTJDh-0",
+                    url: "https://maps.googleapis.com/maps/api/geocode/json?components=locality:"+ address +"&key=AIzaSyDymcMD7E0irE6FM1uGqzEhVmU5LTJDh-0",
                     success: function(response){
 
                     }
